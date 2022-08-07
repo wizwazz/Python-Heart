67d768f81f2f1073eddfe709862b5aec3a174b27
wizwazz <60882873+wizwazz@users.noreply.github.com>
2022-08-08 00:56:44 +0100

    Add files via upload

---
+++ heart.py
@@ -0,0 +1,15 @@
+from turtle import *
+color("black")
+begin_fill()
+pensize(5)
+left(30)
+forward(133)
+circle(50, 200)
+right(140)
+circle(50, 200)
+forward(133)
+fillcolor("red")
+end_fill()
+#Programmed By Wazz
+#18.03.22
+
